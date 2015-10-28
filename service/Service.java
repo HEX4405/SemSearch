609310b0f158b015ad3b18eb99786efd5609e40b
thomasfernandez <fernandez.thomas.f@gmail.com>
2015-10-28 01:11:48 +0100

    Put a try/catch block for all exception that can be thrown and print the error message on the error output for now.

--- service/Service.java
+++ service/Service.java
@@ -3,13 +3,11 @@ package service;
 import service.extract.Extractor;
 import service.finder.URIFinder;
 import service.search.SearchEngine;
-
-import java.io.IOException;
 import java.util.List;
 
 public class Service {
 
-    public static void main(String [] args) throws IOException {
+    public static void main(String [] args) {
         List<String> urls = SearchEngine.search("obama", 10);
         List<String> texts = Extractor.extract(urls);
         List<List<String>> urisLists = URIFinder.find(texts);
--- service/search/SearchEngine.java
+++ service/search/SearchEngine.java
@@ -15,14 +15,24 @@ public class SearchEngine {
 
     }
 
-    public static List<String> search(String query, int numberOfResults) throws IOException {
+    public static List<String> search(String query, int numberOfResults) {
         List<String> urls = new ArrayList<String>();
-        Document doc = Jsoup.connect("https://www.bing.com/search?q=" + query + "&count=" + numberOfResults).get();
-        Elements elements = doc.select(".b_algo h2 a");
 
-        for(Element e : elements) {
-            String url = e.attr("href");
-            urls.add(url);
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://www.bing.com/search?&q=" + query + "&count=" + numberOfResults + "&lf=1")
+                    .header("Accept-Language", "en-US")
+                    .timeout(0)
+                    .get();
+
+            Elements elements = doc.select(".b_algo h2 a");
+
+            for(Element e : elements) {
+                String url = e.attr("href");
+                urls.add(url);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
         return urls;
     }

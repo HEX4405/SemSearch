87e7b48d0f31b0f2996f64ce7a093434378317df
thomasfernandez <fernandez.thomas.f@gmail.com>
2015-10-28 01:02:31 +0100

    Change signature of the find method. Now it takes a list of texts in parameter and return a List of list of URI (one list per text)

--- service/finder/URIFinder.java
+++ service/finder/URIFinder.java
@@ -1,10 +1,12 @@
 package service.finder;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,19 +17,37 @@ public class URIFinder {
 
     }
 
-    public static List<String> find(String text) throws IOException {
-        List<String> uris = new ArrayList<>();
+    public static List<List<String>> find(List<String> texts) {
+        List<List<String>> urisLists = new ArrayList<>();
 
-        Document doc = Jsoup.connect("http://spotlight.dbpedia.org/rest/annotate")
-                .data("text", URLEncoder.encode(text, "UTF-8"))
-                .data("confidence", "0.2")
-                .data("support", "20")
-                .post();
+        Connection spotlight = Jsoup.connect("http://spotlight.sztaki.hu:2222/rest/annotate")
+                .header("content-type", "application/x-www-form-urlencoded")
+                .data("confidence", "0.5")
+                .data("support", "0")
+                //.data("spotter", "Default")
+                //.data("disambiguator", "Default")
+                //.data("policy", "whitelist")
+                .timeout(0);
 
-        for(Element e : doc.select("a")) {
-            uris.add(e.attr("href"));
+        for(String text : texts) {
+            List<String> uris = new ArrayList<>();
+            String encodedText = null;
+            try {
+                encodedText = URLEncoder.encode(text, "UTF-8");
+                Document doc = spotlight.data("text", encodedText).post();
+
+                for (Element e : doc.select("a")) {
+                    uris.add(e.attr("href"));
+                }
+
+                urisLists.add(uris);
+            } catch (UnsupportedEncodingException e) {
+                System.err.println(e.getMessage());
+            } catch(IOException e) {
+                System.err.println(e.getMessage());
+            }
         }
 
-        return uris;
+        return urisLists;
     }
 }

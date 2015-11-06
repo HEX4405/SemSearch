fbd1fb889eee115531df7ec8c1b89221385d283b
thomasfernandez <fernandez.thomas.f@gmail.com>
2015-11-06 08:59:11 +0100

    First version of the identifyConcepts method that return a list of snippets.

---
+++ modele/Snippet.java
@@ -0,0 +1,16 @@
+package modele;
+
+
+public class Snippet {
+
+    private String title;
+
+    public Snippet(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+}
--- service/Service.java
+++ service/Service.java
@@ -1,49 +1,43 @@
 package service;
 
-import org.apache.jena.rdf.model.Model;
+import modele.Snippet;
 import service.extract.Extractor;
 import service.finder.URIFinder;
 import service.frequency.FrequencySorter;
-import service.graphUnifier.GraphUnifier;
 import service.search.SearchEngine;
-import service.sparql.RDFGraphGenerator;
 
 import java.util.*;
 
 public class Service {
 
-    public static void main(String [] args) {
-
-        long startTime = System.currentTimeMillis();
-
-        searchConcepts("communism", "BING", 5, 0);
-
-        long stopTime = System.currentTimeMillis();
-        long elapsedTime = stopTime - startTime;
-        System.out.println("Execution time: " + elapsedTime + " ms.");
-
-    }
-
-    public static void searchConcepts(String query, String searchEngine, int numberOfResults, double similarity) {
+    public static List<Snippet> identifyConcepts(String query, String searchEngine, int numberOfResults, double similarity) {
         List<String> urls = null;
-        List<String> texts = null;
+        Map<String, String> textsMap = null;
         int currentNumberOfResults = 0;
 
         for(int i = 0; i < 10 && currentNumberOfResults != numberOfResults; i++) {
             urls = SearchEngine.search(searchEngine, query, numberOfResults);
-            texts = Extractor.extract(urls);
-            currentNumberOfResults = texts.size();
+            textsMap = Extractor.extract(urls);
+            currentNumberOfResults = textsMap.size();
         }
 
-        System.out.println("Annotating...");
+        List<String> titles = new ArrayList<>(textsMap.keySet());
+        List<String> texts = new ArrayList<>(textsMap.values());
+
         List<List<String>> urisList = URIFinder.find(texts, 0.2);
-        System.out.println("Frequency sorting...");
         List<List<String>> sortedUrisList = FrequencySorter.processAll(urisList);
-        System.out.println("Graph building...");
-        List<List<Model>> modelsList = RDFGraphGenerator.generateAllRDF(sortedUrisList);
-        //System.out.println("Unifying models...");
+
+        //List<List<Model>> modelsList = RDFGraphGenerator.generateAllRDF(sortedUrisList);
         //List<Model> unifiedModels = GraphUnifier.unifyAllModels(modelsList);
-        System.out.println("Done !");
+
+        List<Snippet> snippets = new ArrayList<>();
+
+        for(int i = 0; i < numberOfResults; i++) {
+            String title = titles.get(i);
+            snippets.add(new Snippet(title));
+        }
+
+        return snippets;
     }
 }
 

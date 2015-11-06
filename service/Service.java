12e253e5532007be8842ddee7245e6f687610f9e
thomasfernandez <fernandez.thomas.f@gmail.com>
2015-11-06 06:23:36 +0100

    Implements the beginning of the searchConcepts method

--- service/Service.java
+++ service/Service.java
@@ -1,34 +1,49 @@
 package service;
 
+import org.apache.jena.rdf.model.Model;
 import service.extract.Extractor;
 import service.finder.URIFinder;
 import service.frequency.FrequencySorter;
+import service.graphUnifier.GraphUnifier;
 import service.search.SearchEngine;
+import service.sparql.RDFGraphGenerator;
 
 import java.util.*;
 
 public class Service {
 
     public static void main(String [] args) {
-        String query = "communism";
-        int numberOfResults = 10;
-        double confidence = 0.2;
 
-        System.out.println("Searching...");
-        List<String> urls = SearchEngine.search(SearchEngine.Engine.BING, query, numberOfResults);
-        System.out.println("Extracting...");
-        List<String> texts = Extractor.extract(urls);
-        System.out.println("Finding URI...");
-        List<List<String>> urisList = URIFinder.find(texts, confidence);
-        System.out.println("Done.");
-        List<List<String>> sortedUrisList = FrequencySorter.processAll(urisList);
+        long startTime = System.currentTimeMillis();
 
-        for(List<String> sortedUris : sortedUrisList) {
-            for(int i = 0; i < sortedUris.size() && i < 10; i++) {
-                System.out.println(sortedUris.get(i));
-            }
-            System.out.println("============");
+        searchConcepts("communism", "BING", 5, 0);
+
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println("Execution time: " + elapsedTime + " ms.");
+
+    }
+
+    public static void searchConcepts(String query, String searchEngine, int numberOfResults, double similarity) {
+        List<String> urls = null;
+        List<String> texts = null;
+        int currentNumberOfResults = 0;
+
+        for(int i = 0; i < 10 && currentNumberOfResults != numberOfResults; i++) {
+            urls = SearchEngine.search(searchEngine, query, numberOfResults);
+            texts = Extractor.extract(urls);
+            currentNumberOfResults = texts.size();
         }
+
+        System.out.println("Annotating...");
+        List<List<String>> urisList = URIFinder.find(texts, 0.2);
+        System.out.println("Frequency sorting...");
+        List<List<String>> sortedUrisList = FrequencySorter.processAll(urisList);
+        System.out.println("Graph building...");
+        List<List<Model>> modelsList = RDFGraphGenerator.generateAllRDF(sortedUrisList);
+        //System.out.println("Unifying models...");
+        //List<Model> unifiedModels = GraphUnifier.unifyAllModels(modelsList);
+        System.out.println("Done !");
     }
 }
 

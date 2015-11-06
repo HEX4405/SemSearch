f1f3408b4508d3ae79a68d74ba1c428b6dd5f693
fcallet <francois.callet@insa-lyon.fr>
2015-11-06 02:00:45 +0100

    improving concordance matrix

--- service/Service.java
+++ service/Service.java
@@ -4,27 +4,50 @@ import service.extract.Extractor;
 import service.finder.URIFinder;
 import service.frequency.FrequencySorter;
 import service.search.SearchEngine;
+import service.similarity.Similarity;
+import service.sparql.RDFGraphGenerator;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
 
 import java.util.*;
 
 public class Service {
 
     public static void main(String [] args) {
-        System.out.println("Searching...");
+        /*System.out.println("Searching...");
         List<String> urls = SearchEngine.search(SearchEngine.Engine.GOOGLE, "communism", 10);
         System.out.println("Extracting...");
         List<String> texts = Extractor.extract(urls);
         System.out.println("Finding URI...");
         List<List<String>> urisLists = URIFinder.find(texts, 0.5);
         System.out.println("Done.");
-
-        for(List<String> uris : urisLists) {
-           List<String> sortedUris = FrequencySorter.process(uris, 1);
-           for(String uri : sortedUris) {
-               System.out.println(uri);
-           }
-           System.out.println("===========");
-        }
+        
+        
+        List<List<Model>> modelsByUri = new ArrayList<>();*/
+        
+        Model m = ModelFactory.createDefaultModel();
+		Model m1 = ModelFactory.createDefaultModel();
+		List<String> uris = new ArrayList<String>();
+		uris.add("http://dbpedia.org/resource/Medium-security");
+		uris.add("http://dbpedia.org/resource/Medium-security");
+		Property property = m.createProperty("vCard:FN");
+		m.createResource("http://somewhere/JohnSmith").addProperty(property, "John Smith");
+		m1.createResource("http://somewhere/JohnSmith").addProperty(property, "John Smith");
+		m1.createResource("http://somewhere/JohnWick").addProperty(property, "John Wick");
+		List<Model> models = new ArrayList<Model>();
+		//models.add(m);
+		//models.add(m1);
+		models = RDFGraphGenerator.generateRDF(uris);
+		int n = models.size();
+		float[][] result = new float[n][n];
+		result = Similarity.getMatrixSimilarity(models);
+		for(int i = 0; i<n;i++){
+			for(int j = 0; j<n;j++){
+				System.out.print(result[i][j]+" ");
+			}
+			System.out.print("\n");
+		}
     }
 }
 
--- service/similarity/Similarity.java
+++ service/similarity/Similarity.java
@@ -3,10 +3,6 @@ package service.similarity;
 import java.util.List;
 
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 
 public class Similarity {
@@ -14,37 +10,26 @@ public class Similarity {
 	private Similarity() {
     }
 	
+	private static float getNumberTripls(StmtIterator it){
+		float result = 0;
+		while (it.hasNext()) {
+		    it.nextStatement();
+		    result++;
+		}
+		return result;
+	}
 	
 	private static float getConcordance(Model model1, Model model2) {
-		StmtIterator iter1 = model1.listStatements();
-		StmtIterator iter2 = model2.listStatements();
 		
-		float cardinalUnion = 0;
-		float cardinalIntersection = 0;
-
-		while (iter1.hasNext()) {
-			cardinalUnion++;
-			
-		    Statement stmt1      = iter1.nextStatement();
-		    Resource  subject1   = stmt1.getSubject();
-		    Property  predicate1 = stmt1.getPredicate();
-		    RDFNode   object1    = stmt1.getObject();
-		    while(iter2.hasNext()) {
-		    	cardinalUnion++;
-		    	
-		    	Statement stmt2      = iter2.nextStatement();
-			    Resource  subject2   = stmt2.getSubject();
-			    Property  predicate2 = stmt2.getPredicate();
-			    RDFNode   object2    = stmt2.getObject();
-			    
-			    
-			    if(subject1.equals(subject2) && predicate1.equals(predicate2) && object1.equals(object2)) {
-			    	cardinalUnion--;
-			    	cardinalIntersection++;
-			    }
-		    }
-		    
-		}
+		Model union = model1.union(model2);
+		Model intersection = model1.intersection(model2);
+		
+		StmtIterator iter1 = union.listStatements();
+		StmtIterator iter2 = intersection.listStatements();
+		
+		float cardinalUnion = Similarity.getNumberTripls(iter1);
+		float cardinalIntersection = Similarity.getNumberTripls(iter2);
+		
 		return cardinalIntersection/cardinalUnion;
 	}
 
